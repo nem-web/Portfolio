8606e41d8b88c4a6eb478c68734690b6dc8d1424
nem.web <nemchand.student.eee23@itbhu.ac.in>
2025-07-06 23:01:57 +0530

    Force commit: updated login logic and added affiliate

--- src/affiliate/firebase.js
+++ src/affiliate/firebase.js
@@ -2,18 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXsfJU-_YrJssJdArVd4jFh3luynJho38",
-  authDomain: "affiliate-63f05.firebaseapp.com",
-  projectId: "affiliate-63f05",
-  storageBucket: "affiliate-63f05.firebasestorage.app",
-  messagingSenderId: "612532336953",
-  appId: "1:612532336953:web:172c3fc098631e4a4e10e8",
-  measurementId: "G-4HLX73RZN6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 const db = getFirestore(app);
 
 export { db };
